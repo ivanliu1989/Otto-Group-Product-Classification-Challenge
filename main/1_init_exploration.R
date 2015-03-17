f66aed93cc6d0c402f6d4327f7edfc216ea86413
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-18 09:32:24 +1100

    first try

--- main/1_init_exploration.R
+++ main/1_init_exploration.R
@@ -19,4 +19,10 @@ head(train);colnames(train)
 
 fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 2, classProbs = TRUE,
                         summaryFunction = twoClassSummary, adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
+gbmGrid <-  expand.grid(mtry=17)
 fit <- train(x = train[,c(2:94)], y = as.factor(train[,95]), method = "rf",metric = 'ROC',trControl = fitControl,tuneLength = 8) #95:103
+# tuneGrid = gbmGrid
+
+library('e1071')
+model <- svm(target~., train)
+res <- predict(model, newdata=train)

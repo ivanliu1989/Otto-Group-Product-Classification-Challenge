63b6b495a86a49887036f9a4dd8c0e90f8e704f2
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-18 09:40:24 +1100

    first try

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Otto-Group-Product-Classification-Challenge
+Otto-Group-Product-Classification-Challenge
+==============================================
 For this competition, Otto Group has provided a dataset with 93 features for more than 200,000 products. The objective is to build a predictive model which is able to distinguish between their main product categories. 
 
 ![alt tag](https://kaggle2.blob.core.windows.net/competitions/kaggle/4280/media/Grafik.jpg)
+
+#### BENCHMARK TRY
+1. random forest
+2. svm
+3. logistic regression
+4. nnet	
--- main/1_init_exploration.R
+++ main/1_init_exploration.R
@@ -7,22 +7,21 @@ require(data.table);require(caret)
 # train <- as.data.frame(fread(unzip('data/train.csv.zip'),stringsAsFactors = F))
 # test <- as.data.frame(fread(unzip('data/test.csv.zip'),stringsAsFactors = F))
 # sampleSubmission <- fread('data/sampleSubmission.csv',stringsAsFactors = F)
-# save(train,test,sampleSubmission, file='data/raw_data.RData')
-load(file='data/raw_data.RData')
+# save(train,test, file='data/raw_data_multi.RData')
+load(file='data/raw_data_multi.RData')
 
-levels(as.factor(train$target))
-dummies <- dummyVars(~target, data = train)
-target <- predict(dummies, newdata = train)
-train$target <- NULL
-train <- cbind(train, target)
+# levels(as.factor(train$target))
+# dummies <- dummyVars(~target, data = train)
+# target <- predict(dummies, newdata = train)
+# train$target <- NULL
+# train <- cbind(train, target)
 head(train);colnames(train)
 
-fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 2, classProbs = TRUE,
-                        summaryFunction = twoClassSummary, adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
+fitControl <- trainControl(method = "none", number = 10, repeats = 2, classProbs = TRUE,
+                           summaryFunction = twoClassSummary, adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
 gbmGrid <-  expand.grid(mtry=17)
-fit <- train(x = train[,c(2:94)], y = as.factor(train[,95]), method = "rf",metric = 'ROC',trControl = fitControl,tuneLength = 8) #95:103
-# tuneGrid = gbmGrid
+fit <- train(x = train[,c(2:94)], y = as.factor(train[,95]), method = "rf",metric = 'ROC',trControl = fitControl,tuneLength = 8,tuneGrid = gbmGrid) #95:103
 
 library('e1071')
-model <- svm(target~., train)
-res <- predict(model, newdata=train)
+model <- svm(as.factor(target)~., train)
+res <- predict(model, newdata=train,type = "prob")

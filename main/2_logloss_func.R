50ca7bb0c99b7224b9f8fe0f0ef994042294fd14
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-18 11:23:26 +1100

    first try

--- main/2_logloss_func.R
+++ main/2_logloss_func.R
@@ -3,6 +3,7 @@ logloss <- function(pred, target = target){
     n <- nrow(pred)
     m <- 9
     p <- 0
+    #P <- mapply(function(pre,tar){p <- tar * max(min(pre,1−10^−15),10^−15) + p}, pred, target)
     for(i in 1:n){
         for(j in 1:m){
             p <- target[i,j] * max(min(pred[i,j],1−10^−15),10^−15) + p

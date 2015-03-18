c75f0ae7163b3a87c4477c160e5f53a912352494
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-19 09:59:21 +1100

    rescale

--- main/2_modelling.R
+++ main/2_modelling.R
@@ -1,6 +1,6 @@
-setwd('H:/Machine_Learning/Otto-Group-Product-Classification-Challenge')
+# setwd('H:/Machine_Learning/Otto-Group-Product-Classification-Challenge')
 setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge')
-setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
+# setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
 rm(list=ls());gc()
 require(caret)
 source('main/2_logloss_func.R')
@@ -18,24 +18,24 @@ trainIndex <- createDataPartition(train[,95], p = .7,list = FALSE)
 train_df <- train[trainIndex,]
 test_df  <- train[-trainIndex,]
 
-# fitControl <- trainControl(method = "none", number = 10, repeats = 3, classProbs = TRUE,
-#                            adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
-fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
-gbmGrid <-  expand.grid(n.trees = 150, interaction.depth = 3, shrinkage = 0.1) 
-fit <- train(x = train[,c(2:94)], y = as.factor(train[,95]), method ="gbm", metric ='Kappa', 
-             trControl = fitControl,tuneGrid = gbmGrid) #Accuracy Kappa tuneLength = 8,
+fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 3, classProbs = TRUE,
+                           adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
+# fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
+# gbmGrid <-  expand.grid(mtry=17)  #n.trees = 50, interaction.depth = 1, shrinkage = 0.1
+fit <- train(x = train_df[,c(2:94)], y = as.factor(train_df[,95]), method ="rf", metric ='Kappa', 
+             trControl = fitControl,do.trace=100, importance = TRUE) #Accuracy Kappa tuneLength = 8,tuneGrid = gbmGrid, 
 
 # trellis.par.set(caretTheme())
 # plot(fit, metric = "Kappa")
-val <- predict(fit, newdata=train,type = "prob")
-# target_df <- target[-trainIndex,]
+val <- predict(fit, newdata=test_df,type = "prob")
+target_df <- target[-trainIndex,]
 # confusionMatrix(val,target_df)
 # table(apply(val,1,sum))
-LogLoss(target_df,val)
+LogLoss(target,val)
 
 res <- predict(fit, newdata=test,type = "prob")
 submission <- cbind(id=test$id, res)
-write.csv(submission,file='../first_try_gbm.csv',row.names=F)
+write.csv(submission,file='../first_try_rf.csv',row.names=F)
 
 ######################
 ### Tuning Results ###

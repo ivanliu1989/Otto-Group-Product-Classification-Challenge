f1be22a9fb8112d47332b514adbef71eb22e9ace
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-19 09:15:01 +1100

    rescale

--- main/2_modelling.R
+++ main/2_modelling.R
@@ -3,7 +3,8 @@ setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge')
 setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
 rm(list=ls());gc()
 require(caret)
-
+source('main/2_logloss_func.R')
+load(file='data/target.RData')
 load(file='data/raw_data_multi.RData')
 table(train$target)
 
@@ -21,18 +22,15 @@ test_df  <- train[-trainIndex,]
 #                            adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
 fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
 gbmGrid <-  expand.grid(n.trees = 150, interaction.depth = 3, shrinkage = 0.1) 
-fit <- train(x = train_df[,c(2:94)], y = as.factor(train_df[,95]), method ="gbm", metric ='Kappa', 
+fit <- train(x = train[,c(2:94)], y = as.factor(train[,95]), method ="gbm", metric ='Kappa', 
              trControl = fitControl,tuneGrid = gbmGrid) #Accuracy Kappa tuneLength = 8,
 
-trellis.par.set(caretTheme())
-plot(fit, metric = "Kappa")
-
-val <- predict(fit, newdata=test_df,type = "prob")
-source('main/2_logloss_func.R')
-load(file='data/target.RData')
-target_df <- target[-trainIndex,]
+# trellis.par.set(caretTheme())
+# plot(fit, metric = "Kappa")
+val <- predict(fit, newdata=train,type = "prob")
+# target_df <- target[-trainIndex,]
 # confusionMatrix(val,target_df)
-table(apply(val,1,sum))
+# table(apply(val,1,sum))
 LogLoss(target_df,val)
 
 res <- predict(fit, newdata=test,type = "prob")

b882290cc7ccfef81254fa9edec841d394738041
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-19 22:40:27 +1100

    semi-supervise

--- main/2_modelling.R
+++ main/2_modelling.R
@@ -20,12 +20,12 @@ train_df <- train[trainIndex,]
 test_df  <- train[-trainIndex,]
 train_df <-train
 
-# fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 3, classProbs = TRUE,
-#                            adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
-fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
-gbmGrid <-  expand.grid(mtry=17)  #n.trees = 50, interaction.depth = 1, shrinkage = 0.1
-fit <- train(x = train_df[,c(2:81,83:91)], y = as.factor(train_df[,82]), method ="rf", metric ='Kappa', 
-             trControl = fitControl,do.trace=100, importance = TRUE,tuneGrid = gbmGrid) #Accuracy Kappa,tuneLength = 8, repeats = 15,preProc = c("center","scale","pca")
+fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 3, classProbs = TRUE,
+                           adaptive = list(min = 10,alpha = 0.05,method = 'BT',complete = TRUE))
+# fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
+# gbmGrid <-  expand.grid(mtry=17)  #n.trees = 50, interaction.depth = 1, shrinkage = 0.1
+fit <- train(x = train_df[,c(2:94)], y = as.factor(train_df[,95]), method ="avNNet", metric ='Kappa', 
+             trControl = fitControl,do.trace=100, importance = TRUE,tuneLength = 10, repeats = 15,preProc = c("center","scale","pca")) #tuneGrid = gbmGrid,Accuracy Kappa,tuneLength = 8, repeats = 15,preProc = c("center","scale","pca")
 
 # trellis.par.set(caretTheme())
 # plot(fit, metric = "Kappa")

a1335d3e3cf7ff01b13c291c54276da9d689166e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-21 19:29:53 +1100

    update

--- main/3_xgboost.R
+++ main/3_xgboost.R
@@ -1,16 +1,24 @@
-require(xgboost)
-require(methods)
+setwd('H:/Machine_Learning/Otto-Group-Product-Classification-Challenge')
+setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge')
+# setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
+rm(list=ls());gc()
+require(caret);require(methods);require(xgboost)
+source('main/2_logloss_func.R')
+load(file='data/target.RData');load(file='data/raw_data_multi.RData')
 
-train = read.csv('data/train.csv',header=TRUE,stringsAsFactors = F)
-test = read.csv('data/test.csv',header=TRUE,stringsAsFactors = F)
-train = train[,-1]
-test = test[,-1]
+dim(train);set.seed(888)
+trainIndex <- createDataPartition(train[,95], p = .7,list = FALSE)
+train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
+
+
+train = train_df[,-1]
+test = test_df[,-1]
 
 y = train[,ncol(train)]
 y = gsub('Class_','',y)
 y = as.integer(y)-1 #xgboost take features in [0,numOfClass)
 
-x = rbind(train[,-ncol(train)],test)
+x = rbind(train[,-ncol(train)],test[,-ncol(test)])
 x = as.matrix(x)
 x = matrix(as.numeric(x),nrow(x),ncol(x))
 trind = 1:length(y)
@@ -20,24 +28,32 @@ teind = (nrow(train)+1):nrow(x)
 param <- list("objective" = "multi:softprob",
               "eval_metric" = "mlogloss",
               "num_class" = 9,
-              "nthread" = 8)
+              "nthread" = 4)
 
 # Run Cross Valication
 cv.nround = 50
 bst.cv = xgb.cv(param=param, data = x[trind,], label = y, 
-                nfold = 3, nrounds=cv.nround)
+                nfold = 10, nrounds=cv.nround)
 
 # Train the model
-nround = 50
-bst = xgboost(param=param, data = x[trind,], label = y, nrounds=nround)
+bst = xgboost(param=param, data = x[trind,], label = y, max.depth = 6, eta = 0.1, nround = 150, gamma = 0.3, subsample=1)
 
 # Make prediction
 pred = predict(bst,x[teind,])
 pred = matrix(pred,9,length(pred)/9)
 pred = t(pred)
 
+# Validation
+target_df <- target[-trainIndex,]
+LogLoss(target_df,pred)
+
 # Output submission
 pred = format(pred, digits=2,scientific=F) # shrink the size of submission
 pred = data.frame(1:nrow(pred),pred)
 names(pred) = c('id', paste0('Class_',1:9))
 write.csv(pred,file='submission.csv', quote=FALSE,row.names=FALSE)
+
+# 0.53
+# 0.5252165
+# 0.498124 nround=150
+# 0.4968521 max.depth=6, eta=0.3, nround=150, gamma=0.3, subsample=1

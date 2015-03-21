7eb0dfca97f499faabaffc84910479ebfc0e6a30
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-21 23:27:18 +1100

    xgboost;

--- main/3_xgboost.R
+++ main/3_xgboost.R
@@ -5,11 +5,11 @@ rm(list=ls());gc()
 require(caret);require(methods);require(xgboost)
 source('main/2_logloss_func.R')
 load(file='data/target.RData');load(file='data/raw_data_multi.RData')
-
+# train = train[,-1]
+# test = test[,-1]
 dim(train);set.seed(888)
 trainIndex <- createDataPartition(train[,95], p = .7,list = FALSE)
 train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
-
 
 train = train_df[,-1]
 test = test_df[,-1]
@@ -38,30 +38,35 @@ bst.cv = xgb.cv(param=param, data = dtrain, label = y,
                 nfold = 10, nrounds=cv.nround)
 
 # Train the model
-set.seed(888)
+set.seed(88)
 bst = xgboost(param=param, data = dtrain, label = y, max.depth = 6, eta = 0.1, nround = 500, gamma = 0.1, subsample=0.8)
 
 # Make prediction
 pred = predict(bst,dtest)
 pred = matrix(pred,9,length(pred)/9)
 pred = t(pred)
-pred3 <- pred
+pred1 <- pred
 # pred_ensemble <- (pred1 + pred2 + pred3 + pred4 + pred5)/5
+# for (i in 1:9){
+#     for (j in 1:nrow(pred1)){
+#         pred_ensemble[j,i] <- max(pred1[j,i],pred2[j,i],pred3[j,i],pred4[j,i],pred5[j,i])
+#     }
+# }
 
 # Validation
 target_df <- target[-trainIndex,]
-LogLoss(target_df,pred3)
+LogLoss(target_df,pred1)
 
-ptrain <- predict(bst, dtrain, outputmargin=TRUE)
-ptest  <- predict(bst, dtest, outputmargin=TRUE)
-setinfo(dtrain, "base_margin", ptrain)
-setinfo(dtest, "base_margin", ptest)
+# ptrain <- predict(bst, dtrain, outputmargin=TRUE)
+# ptest  <- predict(bst, dtest, outputmargin=TRUE)
+# setinfo(dtrain, "base_margin", ptrain)
+# setinfo(dtest, "base_margin", ptest)
 
 # Output submission
-pred = format(pred, digits=2,scientific=F) # shrink the size of submission
-pred = data.frame(1:nrow(pred),pred)
-names(pred) = c('id', paste0('Class_',1:9))
-write.csv(pred,file='submission.csv', quote=FALSE,row.names=FALSE)
+pred_ensemble = format(pred_ensemble, digits=2,scientific=F) # shrink the size of submission
+pred_ensemble = data.frame(1:nrow(pred_ensemble),pred_ensemble)
+names(pred_ensemble) = c('id', paste0('Class_',1:9))
+write.csv(pred_ensemble,file='submission_max.csv', quote=FALSE,row.names=FALSE)
 
 
 # 0.4968521 max.depth=6, eta=0.3, nround=150, gamma=0.3, subsample=1
@@ -79,3 +84,4 @@ write.csv(pred,file='submission.csv', quote=FALSE,row.names=FALSE)
 # 0.4843841 max.depth = 6, eta = 0.1, nround = 500, gamma = 0.1, subsample=0.7
 # 0.4814787 max.depth = 6, eta = 0.1, nround = 500, gamma = 0.1, subsample=0.8
 # 0.4762001 pred3
+# 0.4751727 /5

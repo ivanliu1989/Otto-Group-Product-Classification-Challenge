73328d595835d32db31659969d99dfe719a00fff
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-22 21:02:38 +1100

    glmnet

--- main/3_avNNet.R
+++ main/3_avNNet.R
@@ -2,7 +2,7 @@ setwd('H:/Machine_Learning/Otto-Group-Product-Classification-Challenge')
 # setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge')
 # setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
 rm(list=ls());gc()
-require(caret)
+require(caret);require(nnet)
 source('main/2_logloss_func.R')
 load(file='data/target.RData');load(file='data/raw_data_multi.RData')
 
@@ -12,9 +12,9 @@ train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
 # train_df <- train
 
 fitControl <- trainControl(method = "none", number = 10, repeats = 5, classProbs = T, verbose = T)
-gbmGrid <-  expand.grid(size=2, decay=0.5, bag=T)
-fit <- train(x = train_df[,c(2:94)], y = as.factor(train_df[,95]), method ="avNNet",# metric ='Kappa', 
-             trControl = fitControl,do.trace=100, tuneGrid = gbmGrid, repeats = 15, 
+gbmGrid <-  expand.grid(size=8, decay=0.3, maxit = 300)# bag=T)
+fit <- train(x = train_df[,c(2:94)], y = as.factor(train_df[,95]), method ="nnet",# metric ='Kappa', 
+             trControl = fitControl,do.trace=100, tuneGrid = gbmGrid,# repeats = 15, 
              trace=T)#, preProc = c("center","scale",'pca'))
 
 val <- predict(fit, newdata=test_df[,-c(1,95)],type = "prob")
@@ -35,3 +35,9 @@ write.csv(submission,file='../first_try_rf.csv',row.names=F)
 # 0.8665756 size=3, decay=0.1, bag=T, no pca
 # 0.8574749  size=3, decay=0.1, repeats= 15, bag=T, no pca
 # 0.8305999
+# 0.8271034 size=4, decay=0.5, bag=T
+# 0.8072769 size=4, decay=0.3, bag=T
+# 0.7977291 size=4, decay=0.1, bag=T
+# size=4, decay=0.15, bag=T
+
+# 0.6609466 'nnet' size=7, decay=1
--- main/3_glmnet.R
+++ main/3_glmnet.R
@@ -4,17 +4,35 @@ setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge')
 rm(list=ls());gc()
 require(caret);require(glmnet)
 source('main/2_logloss_func.R')
-load(file='data/target.RData');load(file='data/raw_data_multi.RData')
+load(file='data/target.RData')
+load(file='data/raw_data_multi.RData')
+# load(file='data/raw_data_PCA.RData')
 
 dim(train);set.seed(888)
-trainIndex <- createDataPartition(train[,95], p = .7,list = FALSE)
-train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
+# trainIndex <- createDataPartition(train$target, p = .7,list = FALSE)
+# train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
+
+train = train[,-which(names(train) %in% c("id"))] #train
+test = test[,-which(names(test) %in% c("id"))] #test
+
+y = train[,'target']
+y = gsub('Class_','',y)
+y = as.integer(y)-1 #xgboost take features in [0,numOfClass)
+
+x = rbind(train[,-which(names(train) %in% c("target"))],test)#[,-which(names(test) %in% c("target"))])
+x = as.matrix(x)
+x = matrix(as.numeric(x),nrow(x),ncol(x))
+trind = 1:length(y)
+teind = (nrow(train)+1):nrow(x)
+dtrain <- x[trind,]
+dtest <- x[teind,]
+
 # train_df <- train
 
-fit <- glmnet(as.factor(target) ~ ., data=train_df[,-1], family="multinomial",alpha=0.5,standardize=T,
+fit <- glmnet(y=y, x=dtrain, family="multinomial",alpha=0.5,standardize=T,
               type.logistic="Newton",type.multinomial="grouped")
 #family="mgaussian" , #alpha=1 is the lasso penalty, and alpha=0 the ridge penalty
-val <- predict(fit, newdata=test_df,type = "prob")
+val <- predict(fit, newdata=dtest,type = "prob")
 target_df <- target[-trainIndex,]
 LogLoss(target_df,val)
 

c84c22d81cd1c1c358c9742a108d2943f7ae85d8
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-24 13:26:37 +1100

    h2o

--- main/3_h2o_deeplearning.R
+++ main/3_h2o_deeplearning.R
@@ -2,32 +2,42 @@ setwd('H:/Machine_Learning/Otto-Group-Product-Classification-Challenge');
 setwd('/Users/ivan/Work_directory/Otto-Group-Product-Classification-Challenge');
 setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/Otto-Group-Product-Classification-Challenge')
 rm(list=ls());gc()
-library(h2o)
+library(h2o);require(caret)
 source('main/2_logloss_func.R')
 load(file='data/target.RData')
 load(file='data/raw_data_multi.RData')
 
 localH2O <- h2o.init(ip = 'localhost', port = 54321, max_mem_size = '8g')
 h2o.clusterInfo(localH2O)
-train <- as.h2o(localH2O, train, key="train")
-test <- as.h2o(localH2O, test, key="test")
 
-independent <- colnames(train[,2:94])
+trainIndex <- createDataPartition(train$target, p = .7,list = FALSE)
+train_df <- train[trainIndex,];test_df  <- train[-trainIndex,]
+
+train_df <- as.h2o(localH2O, train_df, key="train")
+test_df <- as.h2o(localH2O, test_df, key="test")
+
+independent <- colnames(train_df[,2:94])
 dependent <- "target"
-fit <- h2o.gbm(y = dependent, x = independent, data = train, 
+fit <- h2o.gbm(y = dependent, x = independent, data = train_df, 
                n.trees = 150, interaction.depth = 5,
                n.minobsinnode = 2, shrinkage = 0.01, distribution= "multinomial")
 
-fit <- h2o.deeplearning(y = dependent, x = independent, data = train, 
-                        classification=T,activation="Tanh",
+fit <- h2o.deeplearning(y = dependent, x = independent, data = train_df, 
+                        classification=T,activation="TanhWithDropout",#Tanh
+                        input_dropout_ratio = 0.2,hidden_dropout_ratios = c(0.5,0.5,0.5),
                         hidden=c(100,100),epochs=12,variable_importances=T,
                         override_with_best_model=T,nfolds=10,seed=8,loss='CrossEntropy',
-                        rate=0.1,nesterov_accelerated_gradient=T
-                        ,max_w2=4,shuffle_training_data=T)
-#adaptive_rate=0.9,l1=0.4,l2=0.4,input_dropout_ratio=0.1,rate_decay=0.1,epsilon=0.01,
-pred <- h2o.predict(object = fit, newdata = test)
+                        rate=0.1,nesterov_accelerated_gradient=T,shuffle_training_data=T)
+#adaptive_rate=0.9,l1=0.4,l2=0.4,rate_decay=0.1,epsilon=0.01,max_w2=4,
 
+pred <- h2o.predict(object = fit, newdata = test_df)
 pred_ensemble = format(as.data.frame(pred[,2:10]), digits=2,scientific=F) # shrink the size of submission
+target_df <- target[-trainIndex,]
+MulLogLoss(target_df,pred_ensemble)
+
 pred_ensemble = data.frame(1:nrow(pred_ensemble),pred_ensemble)
 names(pred_ensemble) = c('id', paste0('Class_',1:9))
 write.csv(pred_ensemble,file='../submission_max_047.csv', quote=FALSE,row.names=FALSE)
+
+# 0.511555 c(100,100)
+# 0.5188986 hidden=c(50,50,50)

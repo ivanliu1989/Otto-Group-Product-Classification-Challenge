50eb12ab66f939788c197b1c98e9a84920d7169b
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-25 22:59:10 +1100

    xgboost automatic

--- main/3_xgboost.R
+++ main/3_xgboost.R
@@ -28,19 +28,19 @@ dtest <- data.matrix(x[teind,])
 param <- list("objective" = "multi:softprob",
               "eval_metric" = "mlogloss", 
               "nthread" = 2, set.seed = 168, eta=0.05, gamma = 0.05, #<<============#
-              "num_class" = 9, max.depth=8, min_child_weight=4,
+              "num_class" = 9, max.depth=8, min_child_weight=1,
               subsample=0.8, colsample_bytree = 0.9)
 # max.depth = 8, eta = 0.05, nround = 668, gamma = 0.05, subsample=0.8, colsample_bytree = 0.9
 # reg:logistic | logloss | lambda = 0 (L2) | alpha = 0 (L1) | lambda_bias = 0  
 
 ### Run Cross Valication
 cv.nround = 668
-bst.cv = xgb.cv(param=param, data = dtrain, label = y, nfold = 10, 
-                nrounds=cv.nround,prediction = TRUE)
+# bst.cv = xgb.cv(param=param, data = dtrain, label = y, nfold = 10, 
+#                 nrounds=cv.nround,prediction = TRUE)
 
 ### Train the model ###
 set.seed(168) #<<============#
-# bst = xgboost(param=param, data = dtrain, label = y, nround = cv.nround)
+bst = xgboost(param=param, data = dtrain, label = y, nround = cv.nround)
 
 ### Make prediction ###
 pred = predict(bst,dtest)#, ntreelimit=1
@@ -48,8 +48,8 @@ pred = matrix(pred,9,length(pred)/9)
 pred = t(pred)
 
 ### Ensemble ###
-pred17 <- pred #<<============#
-save(pred17, file='../xgboost_pred17.RData') #<<============#
+# pred17 <- pred #<<============#
+# save(pred17, file='../xgboost_pred17.RData') #<<============#
 # pred_ensemble <- (pred1 + pred2 + pred3 + pred4 + pred5 + pred6 + pred7 + pred8 + pred9 + pred10)/10
 # for (i in 1:9){
 #     for (j in 1:nrow(pred1)){
@@ -59,8 +59,8 @@ save(pred17, file='../xgboost_pred17.RData') #<<============#
 # }
 
 ### Validation ###
-# target_df <- target[-trainIndex,]
-MulLogLoss(target,pred_ensemble)
+target_df <- target[-trainIndex,]
+MulLogLoss(target_df,pred)
 
 ### Output submission ###
 pred_ensemble = format(pred_ensemble, digits=2,scientific=F) # shrink the size of submission
@@ -78,5 +78,8 @@ write.csv(pred_ensemble,file='submission_max_10.csv', quote=FALSE,row.names=FALS
 
 # 0.4709471 max.depth = 8, eta = 0.05, nround = 668, gamma = 0.05, subsample=0.8, colsample_bytree = 0.9
 # 0.4693221 same
+
+# 0.4699977 min_child_weight = 4
+
 # 0.4673917 avg 2
 # 0.4376464 max 2

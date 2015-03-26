55eab68d08de6f3a7935e427285c07f96bfa7b56
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-03-27 07:23:50 +1100

    svm tune

--- main_python/3_modeling_svm.py
+++ main_python/3_modeling_svm.py
@@ -63,7 +63,7 @@ test_y = encoder.fit_transform(test_y)
 train_y = encoder.fit_transform(train_y)
 
 best = 10.    
-for n in [2**-15,2**-13,2**-11,2**-9,2**-7,2**-5,2**-3,2**-1,2,2**3]:
+for n in [0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5]:
     #clf= svm.LinearSVC(multi_class='crammer_singer',loss='l2',C=1,penalty='l2',tol=1e-4,dual=False,fit_intercept=True,intercept_scaling=1)
     clf= SVC(C=1, kernel='rbf', degree=3, gamma=n, shrinking=True, probability=True, tol=0.001, cache_size=200, verbose=True, max_iter=-1) #coef0=0.0, class_weight=None, random_state=None
     # ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’    
@@ -81,6 +81,8 @@ for n in [2**-15,2**-13,2**-11,2**-9,2**-7,2**-5,2**-3,2**-1,2,2**3]:
 # C <- [2^-5,2^-3,2^-1,2,2^3,2^5,2^7,2^9,2^11,2^13,2^15] # lose grid => fine grid
 # degree <- [1,2,3]
 # gamma <- [2^-15,2^-13,2^-11,2^-9,2^-7,2^-5,2^-3,2^-1,2,2^3] # lose grid => fine grid
+# C 4000(0.5335)
+# gamma 0.5 (0.5320)
 
 clf = SVC(C=1.0, kernel='rbf', degree=3, gamma=0.0, shrinking=True, probability=True, tol=0.001, cache_size=200, verbose=True, max_iter=-1)
 clf.fit(train,targets)

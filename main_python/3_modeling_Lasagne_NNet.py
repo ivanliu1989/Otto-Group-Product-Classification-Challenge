26ae716161f05f8136c7993be7f45ca401fa9e5d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-07 21:29:23 +1000

    update

--- main_python/3_modeling_Lasagne_NNet.py
+++ main_python/3_modeling_Lasagne_NNet.py
@@ -65,75 +65,75 @@ X = Comb[:num_rows,:]
 X_test = Comb[num_rows:,:]
 
 # Train
-np.random.seed(9)
-
-layers0 = [('input', InputLayer),
-
-           ('dropoutf', DropoutLayer),
-
-           ('dense0', DenseLayer),
-           ('dropout0', DropoutLayer),
-           ('dense1', DenseLayer),
-           ('dropout1', DropoutLayer),
-           ('dense2', DenseLayer),
-           ('dropout2', DropoutLayer),
-           ('output', DenseLayer)]
-           
-net0 = NeuralNet(layers=layers0,                 
-                 input_shape=(None, num_features),
-                 
-                 dropoutf_p=0.5,
-
-                 dense0_num_units=800,
-                 dense0_nonlinearity=leaky_rectify,
-                 #dense0_W=lg.init.Uniform(),
-
-                 dropout0_p=0.35,
-
-                 dense1_num_units=500,
-                 dense1_nonlinearity=leaky_rectify,
-                 #dense1_W=lg.init.Uniform(),
-
-                 dropout1_p=0.35,
-                 
-                 dense2_num_units=300,
-                 dense2_nonlinearity=leaky_rectify,
-                 #dense2_W=lg.init.Uniform(),
-                 
-                 dropout2_p=0.35,
-                 
-                 output_num_units=num_classes,
-                 output_nonlinearity=softmax,
-                 output_W=lg.init.Uniform(),
-
-                 update=nesterov_momentum,
-                 #update=adagrad,
-                 update_learning_rate=theano.shared(float32(0.01)),
-                 update_momentum=theano.shared(float32(0.9)),
-                 
-                 on_epoch_finished=[
-                        AdjustVariable('update_learning_rate', start=0.015, stop=0.001),
-                        AdjustVariable('update_momentum', start=0.9, stop=0.999),
-                        EarlyStopping(patience=30)
-                        ],
-                 
-                 eval_size=0.1,
-                 verbose=1,
-                 max_epochs=200)
-                 
-net0.fit(X, y)
-# 0.489205 200 0.5 150 0.5 100 0.01
-# 0.480746 320 0.5 160 0.5 80 0.01
-# 0.476469 726 0.5 243 0.5 81 0.01 (50)
-# 0.472083 726 0.5 363 0.5 182 0.01 (47)
-# 0.467849 726 0.5 243 0.5 81 0.01 (42)(adjustvariable, earlystopping) 
-# 0.467454 726 0.5 243 0.5 81 0.01 (37)(adjustvariable, earlystopping)
-
-# 0.474071 0.15 1000 0.25 500 0.25 (46)
-# 0.430886 0.15 1000 0.25 500 0.25 (44)
-# 0.467751 0.15 1000 0.25 500 0.25 (28)
-# 0.427678 0.15 800 0.25 500 0.25 300 0.25 (62)
-# 0.423485 0.15 800 0.25 500 0.25 300 0.25 (65) rectify
-
-# Submission 
-make_submission(net0, X_test, ids, encoder, name='../../lasagne_nnet_rect_0.459990.csv')
+for i in range(1,31):
+    
+    np.random.seed(i)
+    
+    layers0 = [('input', InputLayer),
+               ('dropoutf', DropoutLayer),
+               ('dense0', DenseLayer),
+               ('dropout0', DropoutLayer),
+               ('dense1', DenseLayer),
+               ('dropout1', DropoutLayer),
+               ('dense2', DenseLayer),
+               ('dropout2', DropoutLayer),
+               ('output', DenseLayer)]
+               
+    net0 = NeuralNet(layers=layers0,                 
+                     input_shape=(None, num_features),
+                     
+                     dropoutf_p=0.15,
+    
+                     dense0_num_units=800,
+                     dense0_nonlinearity=leaky_rectify,
+                     #dense0_W=lg.init.Uniform(),
+    
+                     dropout0_p=0.25,
+    
+                     dense1_num_units=500,
+                     dense1_nonlinearity=leaky_rectify,
+                     #dense1_W=lg.init.Uniform(),
+    
+                     dropout1_p=0.25,
+                     
+                     dense2_num_units=300,
+                     dense2_nonlinearity=leaky_rectify,
+                     #dense2_W=lg.init.Uniform(),
+                     
+                     dropout2_p=0.25,
+                     
+                     output_num_units=num_classes,
+                     output_nonlinearity=softmax,
+                     output_W=lg.init.Uniform(),
+    
+                     update=nesterov_momentum,
+                     #update=adagrad,
+                     update_learning_rate=theano.shared(float32(0.01)),
+                     update_momentum=theano.shared(float32(0.9)),
+                     
+                     on_epoch_finished=[
+                            AdjustVariable('update_learning_rate', start=0.015, stop=0.001),
+                            AdjustVariable('update_momentum', start=0.9, stop=0.999),
+                            EarlyStopping(patience=30)
+                            ],
+                     
+                     eval_size=0.1,
+                     verbose=1,
+                     max_epochs=200)
+                     
+    net0.fit(X, y)
+    # 0.489205 200 0.5 150 0.5 100 0.01
+    # 0.480746 320 0.5 160 0.5 80 0.01
+    # 0.476469 726 0.5 243 0.5 81 0.01 (50)
+    # 0.472083 726 0.5 363 0.5 182 0.01 (47)
+    # 0.467849 726 0.5 243 0.5 81 0.01 (42)(adjustvariable, earlystopping) 
+    # 0.467454 726 0.5 243 0.5 81 0.01 (37)(adjustvariable, earlystopping)
+    
+    # 0.474071 0.15 1000 0.25 500 0.25 (46)
+    # 0.430886 0.15 1000 0.25 500 0.25 (44)
+    # 0.467751 0.15 1000 0.25 500 0.25 (28)
+    # 0.427678 0.15 800 0.25 500 0.25 300 0.25 (62)
+    # 0.423485 0.15 800 0.25 500 0.25 300 0.25 (65) rectify
+    
+    # Submission 
+    make_submission(net0, X_test, ids, encoder, name='../../nnet/nnet'+str(i)+'.csv')

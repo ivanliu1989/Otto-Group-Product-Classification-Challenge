a3bd0d553efa7dfd10d676c1cc487b16c1794ea7
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-09 08:10:31 +1000

    update

--- Team/Lasagne_NNet.py
+++ Team/Lasagne_NNet.py
@@ -21,26 +21,34 @@ from nolearn.lasagne import NeuralNet
 from adjust_variable import AdjustVariable
 from early_stopping import EarlyStopping
 from sklearn.decomposition import PCA
-
+from sklearn.metrics import log_loss
 
 def load_train_data(path):
-    df = pd.read_csv(path)
-    df.ix[:,1:94] = df.ix[:,1:94].apply(np.log1p)
-    X = df.values.copy()
-    np.random.shuffle(X)
-    X, labels = X[:, 1:-1].astype(np.float32), X[:, -1]
+    train = pd.read_csv(path)  
+    train.ix[:,1:94] = train.ix[:,1:94].apply(np.log1p)
+    #scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
+    #train.ix[:,1:94] = scaler.fit_transform(train.ix[:,1:94])
+    train = train.values.copy()
+    np.random.shuffle(train)
+    ids, train, labels, folds = train[:, 0], train[:, 1:-2].astype(np.float32), train[:, -2], train[:, -1]
+    trainIDX = np.where(folds != 0)
+    testIDX = np.where(folds == 0)
+    X_train = train[trainIDX]
+    X_test = train[testIDX]
+    ids = ids[testIDX]
     encoder = LabelEncoder()
-    y = encoder.fit_transform(labels).astype(np.int32)
-    scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
-    X = scaler.fit_transform(X)
-    return X, y, encoder, scaler
+    labelsPP = encoder.fit_transform(labels)
+    y_train = labelsPP[trainIDX].astype(np.int32)
+    y_test = labelsPP[testIDX].astype(np.int32)
+    
+    return X_train, y_train, X_test, y_test, encoder, scaler, ids
     
 def load_test_data(path, scaler):
     df = pd.read_csv(path)
     df.ix[:,1:94] = df.ix[:,1:94].apply(np.log1p)
     X = df.values.copy()
     X, ids = X[:, 1:].astype(np.float32), X[:, 0].astype(str)
-    X = scaler.transform(X)
+    #X = scaler.transform(X)
     return X, ids
     
 def make_submission(clf, X_test, ids, encoder, name='lasagne_nnet.csv'):
@@ -52,17 +60,20 @@ def make_submission(clf, X_test, ids, encoder, name='lasagne_nnet.csv'):
     print("Wrote submission to file {}.".format(name))
 
 # Load Data    
-X, y, encoder, scaler = load_train_data('../data/train_folds.csv')
-X_test, ids = load_test_data('../../test.csv', scaler)
+X_train, y_train, X_test, y_test, encoder, scaler, testIDS = load_train_data('../data/train_folds.csv')
+Test, ids = load_test_data('../../test.csv', scaler)
 num_classes = len(encoder.classes_)
-num_features = X.shape[1]
+num_features = X_train.shape[1]
 
-num_rows = X.shape[0]
-Comb = np.append(X, X_test, axis=0)
+num_rows = X_train.shape[0]
+num_rows_t = X_test.shape[0]
+Comb = np.append(X_train, X_test, axis=0)
+Comb = np.append(Comb, Test, axis=0)
 pca = PCA()
 Comb = pca.fit_transform(Comb)
-X = Comb[:num_rows,:]
-X_test = Comb[num_rows:,:]
+X_train = Comb[:num_rows,:]
+X_test = Comb[num_rows:(num_rows_t+num_rows),:]
+Test = Comb[(num_rows_t+num_rows):,:]
 
 # Train
 for i in range(1,31):
@@ -75,8 +86,8 @@ for i in range(1,31):
                ('dropout0', DropoutLayer),
                ('dense1', DenseLayer),
                ('dropout1', DropoutLayer),
-               #('dense2', DenseLayer),
-               #('dropout2', DropoutLayer),
+               ('dense2', DenseLayer),
+               ('dropout2', DropoutLayer),
                ('output', DenseLayer)]
                
     net0 = NeuralNet(layers=layers0,                 
@@ -84,7 +95,7 @@ for i in range(1,31):
                      
                      dropoutf_p=0.15,
     
-                     dense0_num_units=1000,
+                     dense0_num_units=800,
                      dense0_nonlinearity=leaky_rectify,
                      #dense0_W=lg.init.Uniform(),
     
@@ -96,11 +107,11 @@ for i in range(1,31):
     
                      dropout1_p=0.25,
                      
-                     #dense2_num_units=300,
-                     #dense2_nonlinearity=leaky_rectify,
+                     dense2_num_units=300,
+                     dense2_nonlinearity=leaky_rectify,
                      #dense2_W=lg.init.Uniform(),
                      
-                     #dropout2_p=0.25,
+                     dropout2_p=0.25,
                      
                      output_num_units=num_classes,
                      output_nonlinearity=softmax,
@@ -117,14 +128,22 @@ for i in range(1,31):
                             EarlyStopping(patience=20)
                             ],
                      
-                     eval_size=0.2,
+                     eval_size=0.1,
                      verbose=1,
                      max_epochs=200)
                      
-    net0.fit(X, y)
+    net0.fit(X_train, y_train)
+    
+    y_prob = net0.predict_proba(X_test)
+    score=log_loss(y_test, y_prob)
+    
+    names = '../../Team_nnet/Val/testPred_Ivan_nnet2_'+str(i)+'_'+ str(score)+'.csv'
+    submission = pd.DataFrame(data=y_prob, index=testIDS)
+    submission.to_csv(names)
+    print("Wrote submission to file {}.".format(names))
+    # Submission 
+    make_submission(net0, Test, ids, encoder, name='../../Team_nnet/Pred/valPred_Ivan_nnet2_'+str(i)+'.csv')
     
     # 0.467751 0.15 1000 0.25 500 0.25 (28)
     # 0.423485 0.15 800 0.25 500 0.25 300 0.25 (65)
     
-    # Submission 
-    make_submission(net0, X_test, ids, encoder, name='../../nnet_2/nnet_'+str(i)+'.csv')
---
+++ Team/xgb.R
@@ -0,0 +1,49 @@
+setwd('/Users/ivanliu/Google Drive/otto/Otto-Group-Product-Classification-Challenge');
+rm(list=ls());gc()
+require(caret);require(methods);require(xgboost);require(data.table)
+source('main_R/2_logloss_func.R');load(file='data/target.RData');
+load(file='data/raw_data_log.RData');
+# train <- fread('../train.csv', header=T, stringsAsFactor = F,data.table=F)
+# test <- fread('../test.csv', header=T, stringsAsFactor = F, data.table=F)
+library(doMC)
+registerDoMC(cores = 3)
+
+train_tot <- cbind(train, target)
+train_tot <- shuffle(train_tot) #<<============#
+train <- train_tot[,1:95]; target <- train_tot[,96:104]
+train = train[,-which(names(train) %in% c("id"))] #train
+test = test[,-which(names(test) %in% c("id"))] #test
+
+y = train[,'target']
+y = gsub('Class_','',y)
+y = as.integer(y)-1 #xgboost take features in [0,numOfClass)
+train <- as.matrix(train[,-94])
+dtrain = matrix(as.numeric(train),nrow(train),ncol(train))
+test <- as.matrix(test[,-1])
+dtest = matrix(as.numeric(test),nrow(test),ncol(test))
+
+for (i in 1:30){
+    seeds <- 8 * i
+    set.seed(seeds) #<<============#
+    param <- list("objective" = "multi:softprob",
+                  "eval_metric" = "mlogloss", 
+                  "nthread" = 3, set.seed = seeds, eta=0.05, gamma = 0.01, #0.05
+                  "num_class" = 9, max.depth=8, min_child_weight=5, 
+                  subsample=0.7, colsample_bytree = 0.6) #0.8
+    cv.nround = 698
+    
+    ### Train the model ###
+    bst = xgboost(param=param, data = dtrain, label = y, nround = cv.nround)
+    
+    ### Make prediction ###
+    pred = predict(bst,dtest)#, ntreelimit=1
+    pred = matrix(pred,9,length(pred)/9)
+    pred = t(pred)
+    
+    pred = format(pred, digits=2,scientific=F) # shrink the size of submission
+    pred = data.frame(1:nrow(pred),pred)
+    names(pred) = c('id', paste0('Class_',1:9))
+    write.csv(pred,file=paste0('../xgboost_2/xgboost_', i, '.csv'), quote=FALSE,row.names=FALSE)
+    print(paste0('Model:',i,' Complete!'))
+}
+

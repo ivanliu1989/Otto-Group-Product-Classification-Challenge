b89d6c0772c04ac7646e0d8f8ac7b6df4f36b7a6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-09 19:20:45 +1000

    update

---
+++ Team/Blending.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 19:18:37 2015
+
+@author: ivanliu
+"""
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+train = pd.read_csv(path)  
+
